5999ddc8d6af90199a0f71bff4b299f33f62426c
Dmitry R <dmr.main@outlook.com>
2022-11-18 13:54:43 +0300

    corrected the code in accordance with pep8 (continued)

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,8 @@ from main import greeting
 
 
 @pytest.mark.parametrize('name,expected', [('Никита', 'Привет, Никита'),
-                                          ('Ольга', 'Привет, Ольга'),
-                                          ],
+                                           ('Ольга', 'Привет, Ольга'),
+                                           ],
                          )
 def test_greeting(name: str, expected: str):
     """Текст приветствия зависит от имени."""
