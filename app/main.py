3e080cf501d59a666f430e4d2de1e5211bc97577
Dmitry R <dmr.main@outlook.com>
2022-11-18 12:18:15 +0300

    corrected the code in accordance with pep8

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
-
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
+    Args:
+    name: Имя пользователя
+    Returns:
+    str: Текст приветствия
+    """
+    return 'Привет, name'
